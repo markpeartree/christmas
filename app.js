10868b6c156339e128a6370fd0b3a4cab13dc97b
markpeartree <mperity@gmail.com>
2019-12-18 17:46:36 +0000

    second

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ router.get('/',function(req,res){
 app.use(express.static('site'))
 app.use(express.static('pics'))
 app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
